9d0f211e9b23225042f9c0b5cb6f09cd5da82ca0
skylerh15 <skylerhiscock@gmail.com>
2016-12-04 23:56:45 -0600

    more work

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ console.log("javascript loaded...");
 $(document).ready(function () {
 	console.log("jquery loaded...");
 	$('#application_form > h3').slice(1).addClass( "ui-state-disabled" );
-	
+	$('#authorizedtoworkinusradio').hide();
+	$('#felony_explanation').hide();
+	$('#previous_worker').hide();
 	
 	$( "#application_form" ).on( "accordionbeforeactivate", function (){
 		return ! arguments[1].newHeader.hasClass( "ui-state-disabled" );
@@ -13,8 +15,7 @@ $(document).ready(function () {
 		width: 75
 	}).selectmenu("menuWidget").addClass("overflow");
 	
-	$('#dateavailable').datepicker({dateFormat: 'mm/dd/yy'});
-	$('#datesigned').datepicker({dateFormat: 'mm/dd/yy'});
+	$('.datepicker').datepicker({dateFormat: 'mm/dd/yy'});
 	
 	$('.applicationradio > input').checkboxradio();
 	
@@ -25,14 +26,43 @@ $(document).ready(function () {
 		heightStyle: "content",
 	});
 	
+	$(".required_label").prepend("*".fontcolor("red"));
 	
-	$("#personal_details").validate({ errorPlacement: function(error, element) {} });
+	
+	$("#personal_details").validate({ errorPlacement: function(error, element) {}});
 	$("#education_details").validate({ errorPlacement: function(error, element) {} });
+	$("#social_security_number").rules("add", {number: true, maxlength: 9, minlength: 9});
+	$("#desired_salary").rules("add", {number: true});
+	$("#whendidyouworkforthiscompany").rules("add", {required: '#haveyouworkedforthiscompany-yes:checked'});
+	$("#felonyexplanation").rules("add", {required: '#haveyoubeenconvictedofafelony-yes:checked'});
+	$("#previous_worker").rules("add", {required: '#haveyouworkedforthiscompany-yes:checked'});
+	
+	$("#uscitizen-no").click(function(){
+		$("#authorizedtoworkinusradio").show();
+	});
+	$("#uscitizen-yes").click(function(){
+		$("#authorizedtoworkinusradio").hide();
+	});
+	
+	$("#haveyouworkedforthiscompany-yes").click(function(){
+		$("#previous_worker").show();
+	});
+	$("#haveyouworkedforthiscompany-no").click(function(){
+		$("#previous_worker").hide();
+	});
+	
+	$("#haveyoubeenconvictedofafelony-no").click(function(){
+		$("#felony_explanation").hide();
+	});
+	$("#haveyoubeenconvictedofafelony-yes").click(function(){
+		$("#felony_explanation").show();
+	});
 	
 	$('#personalProceed').click(function(){
 		
 		//if( $('#personal_details').valid() == true ) {
 			$('#education_tab').removeClass("ui-state-disabled");
+			$("#personal_details_tab").addClass( "ui-state-disabled" );
 			var activeTab = $( "#application_form" ).accordion( "option", "active" );
 			$('#application_form').accordion("option", "active", activeTab + 1);
 		//}
@@ -42,6 +72,7 @@ $(document).ready(function () {
 		
 		//if( $('#education_details').valid() == true ) {
 			$('#references_tab').removeClass("ui-state-disabled");
+			$('#education_tab').addClass("ui-state-disabled");
 			var activeTab = $( "#application_form" ).accordion( "option", "active" );
 			$('#application_form').accordion("option", "active", activeTab + 1);
 		//}
@@ -51,6 +82,7 @@ $(document).ready(function () {
 		
 		//if( $('#education_details').valid() == true ) {
 			$('#previous_employment_tab').removeClass("ui-state-disabled");
+			$('#references_tab').addClass("ui-state-disabled");
 			var activeTab = $( "#application_form" ).accordion( "option", "active" );
 			$('#application_form').accordion("option", "active", activeTab + 1);
 		//}
@@ -59,8 +91,31 @@ $(document).ready(function () {
 		
 		//if( $('#education_details').valid() == true ) {
 			$('#signature_tab').removeClass("ui-state-disabled");
+			$('#previous_employment_tab').addClass("ui-state-disabled");
 			var activeTab = $( "#application_form" ).accordion( "option", "active" );
 			$('#application_form').accordion("option", "active", activeTab + 1);
 		//}
 	});
+	
+	$('#submitApplication').click(function(){
+		$("#dialog-confirm").dialog('open');
+	});
+	
+	$( "#dialog-confirm" ).dialog({
+			autoOpen: false,
+			resizable: false,
+			height: "auto",
+			width: 400,
+			modal: true,
+			buttons: {
+				"Submit Application": function() {
+				$( this ).dialog( "close" );
+			},
+			Cancel: function() {
+			$( this ).dialog( "close" );
+			}
+			}
+	});
+	
 });
+
