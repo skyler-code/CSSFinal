6cbef30993e98737fd9afb0a2c4f4bfd77a1346e
skylerh15 <skylerhiscock@gmail.com>
2016-12-06 00:16:08 -0600

    more work

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
 	$('#authorizedtoworkinusradio').hide();
 	$('#felony_explanation').hide();
 	$('#previous_worker').hide();
+	$('.education_degree').hide();
 	
 	$( "#application_form" ).on( "accordionbeforeactivate", function (){
 		return ! arguments[1].newHeader.hasClass( "ui-state-disabled" );
@@ -16,6 +17,7 @@ $(document).ready(function () {
 	}).selectmenu("menuWidget").addClass("overflow");
 	
 	$('.datepicker').datepicker({dateFormat: 'mm/dd/yy'});
+	$('#datesigned').datepicker('setDate', new Date());
 	
 	$('.applicationradio > input').checkboxradio();
 	
@@ -26,16 +28,23 @@ $(document).ready(function () {
 		heightStyle: "content",
 	});
 	
+	$("input.required").prev('label').prepend("*".fontcolor("red"));
 	$(".required_label").prepend("*".fontcolor("red"));
 	
 	
-	$("#personal_details").validate({ errorPlacement: function(error, element) {}});
+	$("#personal_details").validate({ errorPlacement: function(error, element) {} });
 	$("#education_details").validate({ errorPlacement: function(error, element) {} });
+	$("#reference_details").validate({ errorPlacement: function(error, element) {} });
+	$("#previous_employment_details").validate({ errorPlacement: function(error, element) {} });
+	$("#signature").validate({ errorPlacement: function(error, element) {} });
 	$("#social_security_number").rules("add", {number: true, maxlength: 9, minlength: 9});
 	$("#desired_salary").rules("add", {number: true});
 	$("#whendidyouworkforthiscompany").rules("add", {required: '#haveyouworkedforthiscompany-yes:checked'});
 	$("#felonyexplanation").rules("add", {required: '#haveyoubeenconvictedofafelony-yes:checked'});
 	$("#previous_worker").rules("add", {required: '#haveyouworkedforthiscompany-yes:checked'});
+	$("#highschooldiploma").rules("add", {required: '#highschool-didyougraduate-yes:checked'});
+	$("#collegedegree").rules("add", {required: '#college-didyougraduate-yes:checked'});
+	$("#otherdegree").rules("add", {required: '#other-didyougraduate-yes:checked'});
 	
 	$("#uscitizen-no").click(function(){
 		$("#authorizedtoworkinusradio").show();
@@ -56,6 +65,55 @@ $(document).ready(function () {
 	});
 	$("#haveyoubeenconvictedofafelony-yes").click(function(){
 		$("#felony_explanation").show();
+	});
+	
+	$("#highschool-didyougraduate-yes").click(function(){
+		$("#highschool_diploma").show();
+	});
+	$("#highschool-didyougraduate-no").click(function(){
+		$("#highschool_diploma").hide();
+	});
+	
+	$("#college-didyougraduate-yes").click(function(){
+		$("#college_degree").show();
+	});
+	$("#college-didyougraduate-no").click(function(){
+		$("#college_degree").hide();
+	});
+	
+	$("#other-didyougraduate-yes").click(function(){
+		$("#other_degree").show();
+	});
+	$("#other-didyougraduate-no").click(function(){
+		$("#other_degree").hide();
+	});
+	
+	$('#educationPrevious').click(function(){
+		$('#education_tab').addClass("ui-state-disabled");
+		$("#personal_details_tab").removeClass( "ui-state-disabled" );
+		var activeTab = $( "#application_form" ).accordion( "option", "active" );
+		$('#application_form').accordion("option", "active", activeTab - 1);
+	});
+	
+	$('#referencesPrevious').click(function(){
+		$('#references_tab').addClass("ui-state-disabled");
+		$("#education_tab").removeClass( "ui-state-disabled" );
+		var activeTab = $( "#application_form" ).accordion( "option", "active" );
+		$('#application_form').accordion("option", "active", activeTab - 1);
+	});
+	
+	$('#employersPrevious').click(function(){
+		$('#previous_employment_tab').addClass("ui-state-disabled");
+		$("#references_tab").removeClass( "ui-state-disabled" );
+		var activeTab = $( "#application_form" ).accordion( "option", "active" );
+		$('#application_form').accordion("option", "active", activeTab - 1);
+	});
+	
+	$('#submitPrevious').click(function(){
+		$('#signature_tab').addClass("ui-state-disabled");
+		$("#previous_employment_tab").removeClass( "ui-state-disabled" );
+		var activeTab = $( "#application_form" ).accordion( "option", "active" );
+		$('#application_form').accordion("option", "active", activeTab - 1);
 	});
 	
 	$('#personalProceed').click(function(){
@@ -98,7 +156,9 @@ $(document).ready(function () {
 	});
 	
 	$('#submitApplication').click(function(){
-		$("#dialog-confirm").dialog('open');
+		if( $('#signature').valid() == true ) {
+			$("#dialog-confirm").dialog('open');
+		}
 	});
 	
 	$( "#dialog-confirm" ).dialog({
